389926df9a5bf366cff433559078ac53bcdae3e2
murat cabuk <muratcabuk@live.com>
2024-01-14 15:00:11 +0000

    first

---
+++ message.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char word[50];
+
+    printf("Give me a word: ");
+    scanf("%s", word);
+
+    printf("word: %s\n", word);
+
+    return 0;
+}
